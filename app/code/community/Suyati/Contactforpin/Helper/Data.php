c2cd255d7f6e34bfc1bcbb3e0d07a1538db88928
suyati <nmohanan>
2016-06-13 12:29:37 +0530

    Created Scontact extension files

---
+++ app/code/community/Suyati/Contactforpin/Helper/Data.php
@@ -0,0 +1,21 @@
+<?php
+/*!
+ * http://suyati.com/
+ * Suyati_Contactforpin 1.6.1
+ * Copyright (C) 2016, Suyati Technologies
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+class Suyati_Contactforpin_Helper_Data extends Mage_Core_Helper_Abstract
+{
+	public function getCities() {
+        $connection = Mage::getSingleton('core/resource')->getConnection('core_read');
+        $sql        = "select DISTINCT city_name from saplocation";
+        $rows       = $connection->fetchAll($sql);
+        return $rows;
+    }
+
+
+}
